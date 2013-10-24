562cf3cac8b39de80f4cf9dd3aa38ea2862aaf37
Grandel Cuvos <graandelc@gmail.com>
2013-10-24 14:54:15 -0500

    Chunky

---
+++ app/models/product.rb
@@ -0,0 +1,7 @@
+class Product < ActiveRecord::Base
+  attr_accessible :description, :price, :stock_quantity, :title
+
+  validates :title,           :presence => true
+  validates :price,           :presence => true
+  validates :stock_quantity,  :presence => true
+end
